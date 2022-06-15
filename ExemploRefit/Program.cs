2431255bdca736641564cc0e6ef387fe4376852b
Luan <luanraasch@outlook.com>
2022-06-14 22:41:18 -0300

    first commit

---
+++ ExemploRefit/Program.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    class MainClass
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<iCepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro:{address.Logradouro} \nBairro:{address.Bairro} \nCidade:{address.Localidade} \nIBGE:{address.Ibge}");
+                Console.ReadKey();
+
+            } catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+        }
+    }
+}
---
+++ ExemploRefit/iCepApiService.cs
@@ -0,0 +1,12 @@
+﻿
+using Refit;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    internal interface iCepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
